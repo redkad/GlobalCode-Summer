a2b1f6f5fc46b5c47b7f8c584762f26651439769
redkad <tengecollins@yahoo.com>
2019-07-17 15:51:27 +0000

    intro to flask

--- gpio.py
+++ gpio.py
@@ -1,9 +1,4 @@
-from gpiozero import Button
-button = Button(2)
 import requests
 
-button.when_pressed = requests.post("https://maker.ifttt.com/trigger/glblcd/with/key/mGh-lQN4x9c62thYIg_mkh0Z27glI7okoIl6ilVcWat")
+r = requests.post("https://maker.ifttt.com/trigger/HTU/with/key/mGh-lQN4x9c62thYIg_mkh0Z27glI7okoIl6ilVcWat")
 print(r.status_code)
-
-pause()
- 
